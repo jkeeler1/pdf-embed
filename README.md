752b83a9b4c0fb16925f661beff4b661ef2594bd
Jennifer Keeler <jenniferkeeler@gmail.com>
2024-04-27 17:29:37 -0700

    Got working version of chunking, embedding, and upseting to pinecone.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pdf-embed
-Code to embed pdfs via OpenAI into a Pinecone database
+
+### Overview
+
+This script will take a pdf, break it into text chunks, run it through OpenAI embeddings, and store
+the result in a pinecone database.
+
+This was run with Python 3.12.2
+
+### Usage
+
+`python upsert.py -f /Users/jkeeler/Documents/ai/animals/arctic-fox.pdf -i arctic-fox`
+where:
+ -f is the pdf you wish to read
+ -i is the id that should be associated with
+
+`python upsert.py -h for help`
+
+### Requirements
+Assumes you have set the following environment variables
+* PINECONE_API_KEY
+* OPENAI_API_KEY
+* OPENAI_ORG_ID
+
+#### Light Reading
+https://pypi.org/project/pinecone-client/
+https://docs.pinecone.io/reference/api/data-plane/upsert
--- upsert.py
+++ upsert.py
@@ -1,61 +1,26 @@
 import getopt
 import os
-import re
 import sys
 import openai
 from PyPDF2 import PdfReader
 from pinecone import Pinecone
 
 
-def usage():
-    print('usage: upset.py -f <inputfile> -i <id>')
-
-
-def inputs(argv):
-    # Get the input values
-
-    fname = ''
-    id = ''
-
-    try:
-        opts, args = getopt.getopt(argv, "hf:i:", ["help", "file=", "id="])
-    except getopt.GetoptError:
-        print('usage: upset.py -h')
-        print('usage: upset.py -f <inputfile> -i <id>')
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print('upset.py -f <inputfile> -i <id>')
-            sys.exit()
-        elif opt in ("-f", "--file"):
-            fname = arg
-        elif opt in ("-i", "--id"):
-            id = arg
-
-    print ('filename=', fname)
-    print ('id', id)
-
-    return fname, id
-
 def main(argv):
     # file_path = "/Users/jkeeler/Documents/ai/animals/arctic-fox.pdf"
+    # id = "arctic-fox
     fname, id = inputs(argv)
 
-    # Process a PDF and create embeddings
+    # Break the pdf into chunks
     texts = process_pdf(fname)
 
-    textToEmbedding = create_embeddings(texts)
+    # Create the embeddings from the text chunks
+    embeddings = create_embeddings(id, fname, texts)
 
     # Upsert the embeddings to Pinecone
-    upsert_embeddings_to_pinecone(id, textToEmbedding, fname)
-    print ("Success")
+    upsert_embeddings_to_pinecone(embeddings)
+    print("Success")
 
-# Define a function to preprocess text
-def preprocess_text(text):
-    # Replace consecutive spaces, newlines and tabs
-    text = re.sub(r'\s+', ' ', text)
-    return text
 
 def process_pdf(file_path):
     reader = PdfReader(file_path)
@@ -65,8 +30,7 @@ def process_pdf(file_path):
         texts.append(text)
     return texts
 
-# Define a function to create embeddings
-def create_embeddings(texts):
+def create_embeddings(id, filename, texts):
     # Initialize OpenAI
     MODEL = "text-embedding-ada-002"
     openAIKey = os.environ.get("OPENAI_API_KEY")
@@ -74,34 +38,77 @@ def create_embeddings(texts):
 
     # initialize AI client
     openAIClient = openai.OpenAI(api_key=openAIKey, organization=openAIOrgId)
-    textToEmbedding = {}
+    # list of tuples where each tuple will be one chunk of the file (one row in pinecone)
+    embeddings = []
+    file = extractFilename(filename);
 
-    for text in texts:
+    for index, text in enumerate(texts):
         response = openAIClient.embeddings.create(
             model=MODEL,
             input=text
         )
         embedding = response.data[0].embedding
-        print(embedding)
-        textToEmbedding[text] = embedding
-    return textToEmbedding
+        # print(embedding)
+        chunkId = id + "_" + str(index)
+        embeddings.append((chunkId, embedding, {'name': id, 'filename': file, 'chunk': text}))
+    return embeddings
+
 
 # Define a function to upsert embeddings to Pinecone
-def upsert_embeddings_to_pinecone(id, textToEmbedding, filename):
+def upsert_embeddings_to_pinecone(embeddings):
+
+    if len(embeddings) == 0:
+        return
+
     # Initialize Pinecone
     pineconeKey = os.environ.get("PINECONE_API_KEY")
     pineconeClient = Pinecone(api_key=pineconeKey, environment='gcp-starter')
     index = pineconeClient.Index("animals")
 
-    for fileText, embedding in textToEmbedding.items():
-        print("upserting vector")
-        response = index.upsert(vectors=[
-                {
-                    'id':id,
-                    'values':embedding,
-                    'metadata':{'name':id, 'filename':filename, 'section':fileText}
-                }])
-        print(response)
+    # ("vec1", [0.1, 0.2, 0.3, 0.4], {"genre": "drama"}),
+    response = index.upsert(
+        vectors=embeddings,
+        namespace="animals"
+    )
+    print(response)
+
+# Define a function to create embeddings
+def extractFilename(filename):
+    vals = filename.split("/")
+    return vals[len(vals) - 1]
+
+
+def usage():
+    print('usage: upsert.py -f <inputfile> -i <id>')
+
+
+def inputs(argv):
+    # Get the input values
+
+    fname: str = ''
+    id: str = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hf:i:", ["help", "file=", "id="])
+    except getopt.GetoptError:
+        print('usage: upsert.py -h')
+        print('usage: upsert.py -f <inputfile> -i <id>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print('upsert.py -f <inputfile> -i <id>')
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            fname = arg
+        elif opt in ("-i", "--id"):
+            id = arg
+
+    print('filename=', fname)
+    print('id', id)
+
+    return fname, id
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
